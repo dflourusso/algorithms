a84e4798737a6449d7dc98aa3e67082efedcd08f
Daniel Fernando Lourusso <dflourusso@gmail.com>
2022-05-19 09:36:25 -0300

    feat: resolve binary tree

--- src/invert-binary-tree/index.js
+++ src/invert-binary-tree/index.js
@@ -1,5 +1,13 @@
 const invertBinaryTree = (tree) => {
-  // Your response here
+  if (!tree) return
+  const tmpLeft = tree.left
+  tree.left = tree.right
+  tree.right = tmpLeft
+  
+  invertBinaryTree(tree.left)
+  invertBinaryTree(tree.right)
+
+  return tree  
 }
 
 module.exports = invertBinaryTree
